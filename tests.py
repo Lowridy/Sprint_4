f8a54607b2548b912f1056218f5aa06f54f317a3
LowPWE <username@yandex.ru>
2025-05-06 20:54:28 +0500

    добавлены тесты спринта 4

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
+import pytest
 from main import BooksCollector
+
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -18,7 +20,68 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2 # Похоже в первоначальном примере была опечатка, исправил
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+ # Тест 2: Нельзя добавить дубликат книги
+    def test_add_duplicate_book(self, new_collector):
+        new_collector.add_new_book("Дубликат")
+        new_collector.add_new_book("Дубликат")
+        assert len(new_collector.books_genre) == 1
+
+    # Тест 3: Установка жанра для существующей книги
+    def test_set_book_genre_valid(self, collector_with_books):
+        collector_with_books.set_book_genre("Гарри Поттер", "Мультфильмы")
+        assert collector_with_books.get_book_genre("Гарри Поттер") == "Мультфильмы"
+
+    # Тест 4: Нельзя установить несуществующий жанр
+    def test_set_invalid_genre(self, collector_with_books):
+        collector_with_books.set_book_genre("Гарри Поттер", "Несуществующий жанр")
+        assert collector_with_books.get_book_genre("Гарри Поттер") == "Фантастика"
+
+    # Тест 5: Получение книг по жанру
+    def test_get_books_by_genre(self, collector_with_books):
+        fantasy_books = collector_with_books.get_books_with_specific_genre("Фантастика")
+        assert "Гарри Поттер" in fantasy_books
+        assert len(fantasy_books) == 1
+
+    # Тест 6: Книги для детей (проверка с параметризацией)
+    @pytest.mark.parametrize('book_name, genre, expected', [
+        ('Гарри Поттер', 'Фантастика', True),    # разрешенный жанр
+        ('Оно', 'Ужасы', False),                  # запрещенный жанр
+        ('Ну погоди!', 'Мультфильмы', True),      # детский жанр
+        ('Десять негритят', 'Детективы', False),  # запрещенный жанр
+        ('Книга без жанра', '', False)            # пустой жанр
+    ])
+    def test_get_books_for_children_check_genre_restrictions(self, book_name, genre, expected):
+        collector = BooksCollector()
+        
+        collector.add_new_book(book_name)
+        
+        if genre:
+            collector.set_book_genre(book_name, genre)
+        
+        children_books = collector.get_books_for_children()
+        assert (book_name in children_books) == expected
+
+    # Тест 7: Добавление в избранное
+    def test_add_to_favorites(self, collector_with_books):
+        collector_with_books.add_book_in_favorites("Гарри Поттер")
+        assert "Гарри Поттер" in collector_with_books.get_list_of_favorites_books()
+
+    # Тест 8: Нельзя добавить в избранное дважды
+    def test_add_to_favorites_twice(self, collector_with_favorites):
+        initial_count = len(collector_with_favorites.get_list_of_favorites_books())
+        collector_with_favorites.add_book_in_favorites("Гарри Поттер")  # Пытаемся добавить существующее
+        assert len(collector_with_favorites.get_list_of_favorites_books()) == initial_count
+
+    # Тест 9: Удаление из избранного
+    def test_remove_from_favorites(self, collector_with_favorites):
+        collector_with_favorites.delete_book_from_favorites("Гарри Поттер")
+        assert "Гарри Поттер" not in collector_with_favorites.get_list_of_favorites_books()
+
+    # Тест 10: Получение пустого списка избранного
+    def test_empty_favorites(self, new_collector):
+        assert new_collector.get_list_of_favorites_books() == []
